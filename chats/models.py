bbf09188fc01e4a4c62fc6d2db450dbdd8b2a3a1
Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2021-02-26 18:46:52 +0800

    Adding .gitignore

--- chats/models.py
+++ chats/models.py
@@ -5,13 +5,13 @@ from users.models import Profile
 #Chat may not need model? Will look into it
 #Actual chat text may be stored elsewhere?
 
-class Chat(models.Model):
-    chatID = models.AutoField(primary_key=True)
-    listingID = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    tutor = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="tutor")
-    learner = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="learner"
+# class Chat(models.Model):
+#     chatID = models.AutoField(primary_key=True)
+#     listingID = models.ForeignKey(Listing, on_delete=models.CASCADE)
+#     tutor = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="tutor")
+#     learner = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="learner")
 
-    def __str__(self):
-        return f"{self.chatID}"
+#     def __str__(self):
+#         return f"{self.chatID}"
 
 
