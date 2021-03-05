0d69893c0458cd5cdf647d4574cdac1bf302ba04
guannwei <guanwei58@hotmail.com>
2021-03-05 10:04:03 +0800

    Minor changes
    - Some minor changes on listing templates.
    - Pushed mainapp's views.py and urls.py to test out if there is conflict

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from listings import views as listing_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('listing/view/<int:pk>/', listing_views.ListingDetailView.as_view(), name='listing-detail'),
+    path('listing/createListing/', listing_views.ListingCreateView.as_view(), name='listing-create'),
+    path('listing/update/<int:pk>/', listing_views.ListingUpdateView.as_view(), name='listing-update'),
+    path('listing/delete/<int:pk>/', listing_views.ListingDeleteView.as_view(), name='listing-delete'),
     path('refreshmodules1236172', views.refresh_modules, name='refresh-modules'), # do not go to this URL unnecessarily
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,19 +3,42 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from listings.utility import fetch_modules, populate_modules
+from listings import views as listing_views
 
 # Create your views here.
 
 # home page
 @login_required()
 def home(request):
+    # Get filters
+    studentType = ''
+    if not request.GET.get('tutorFilter', None) == None:
+        studentType = 'Tutor'
+    if not request.GET.get('tuteeFilter', None) == None:
+        studentType = 'Tutee'
 
-    # data to pass into page
-    context = {}
+    tutorFilterQuery = request.GET.get('tutorFilter')
+    tuteeFilterQuery = request.GET.get('tuteeFilter')
+    ratingsFilterQuery = request.GET.get('ratingsFilter')
+    codeFilterQuery = request.GET.get('codeFilter')
+    nameFilterQuery = request.GET.get('nameFilter')
 
-    # render page
-    # defines the template to render and the context to pass into the template
-    return render(request, 'index.html', context)
+    # If filter is none, return as ''
+    if ratingsFilterQuery is None:
+        ratingsFilterQuery = ''
+    if codeFilterQuery is None:
+        codeFilterQuery = ''
+    if nameFilterQuery is None:
+        nameFilterQuery = ''
+
+    context = {
+        'tuitionListings': listing_views.getTuitionListings(studentType, codeFilterQuery, nameFilterQuery, ratingsFilterQuery),
+        'ratingsFilterQuery': ratingsFilterQuery,
+        'codeFilterQuery': codeFilterQuery,
+        'nameFilterQuery': nameFilterQuery
+   }
+
+    return render(request, 'listings/allListings.html', context)
 
 # refresh modules when view is called
 @user_passes_test(lambda user: user.is_superuser)
