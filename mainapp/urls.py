f50f59c42264f601800431725a2c922eec908656
guanwei <guanwei58@hotmail.com>
2021-03-07 11:30:22 +0800

    Revert "Edittted urls.py"
    This reverts commit c31aa5d576b904069dd9a74ff6d4439cdf4dc633.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,5 @@ urlpatterns = [
     path('moderation/approve/<str:username>/', views.moderation_approve, name='approve'),
     path('moderation/reject/<str:username>/', views.moderation_reject, name='reject'),
     path('403/', views.error403, name='error-403'),
-    path('', include('listings.urls')),
     path('refreshmodules1236172/', views.refresh_modules, name='refresh-modules'), # do not go to this URL unnecessarily
 ]
