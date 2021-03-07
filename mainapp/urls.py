44ea465f4cd168a36e4a8cca0af5bb7309fd2264
guanwei <guanwei58@hotmail.com>
2021-03-07 11:33:26 +0800

    Edited mainapp/urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', include('listings.urls')),
     path('moderation/', views.moderation, name='moderation-home'),
     path('moderation/approve/<str:username>/', views.moderation_approve, name='approve'),
     path('moderation/reject/<str:username>/', views.moderation_reject, name='reject'),
     path('403/', views.error403, name='error-403'),
-    path('', include('listings.urls')),
     path('refreshmodules1236172/', views.refresh_modules, name='refresh-modules'), # do not go to this URL unnecessarily
 ]
