9d4bc896b3b394911d5128e6a83419eab45c4ee3
limkv233b <56911828+limkv233b@users.noreply.github.com>
2021-03-08 20:08:23 +0800

    urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('', include('listings.urls')),
 	path('', include('chats.urls')),
+	path('', include('users.urls')),
     path('moderation/', views.moderation, name='moderation-home'),
     path('moderation/approve/<str:username>/', views.moderation_approve, name='approve'),
     path('moderation/reject/<str:username>/', views.moderation_reject, name='reject'),
